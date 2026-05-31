baf8ebabbf2ff83b4028aeaf2f25214f0980b041
albecabrera <albe.cabrera@yahoo.de>
2026-05-31 21:33:47 +0200

    Aprendiendo de String

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
 public static void main(String[] args) {
-    System.out.println("Hola mundo!");
+    System.out.println("Hola Bruno!");
     System.out.println("Hola Java!");
+    /* Das ist ein mehrzeiliges
+    Kommentar. Ich mächte außerdem, dass ich sehr viel Java
+    in den kommenden Monate programieren kann */
     }
 }
---
+++ src/robot.java
@@ -0,0 +1,11 @@
+class HalloAlberto {
+        public static void main(String[] args) {
+                String output = "Hallo Alberto!";
+                System.out.println(output);
+                output = "Hallo Java!";
+                System.out.println(output + "." + "Du lernst ja schon ganz schön viel, das ist super!");
+                 /* Das ist ein mehrzeiliges
+                 Kommentar. Ich mächte außerdem, dass ich sehr viel Java
+                 in den kommenden Monate programieren kann */
+        }
+}
